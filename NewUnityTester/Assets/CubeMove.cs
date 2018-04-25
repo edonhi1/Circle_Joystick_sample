06eafd94e9dd0e8035f135d094ca0c2dc34efd76
edonhi1 <edonhi1@gmail.com>
2018-04-25 17:25:33 +0900

    첫 커밋, 조이스틱테스트

---
+++ NewUnityTester/Assets/CubeMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour {
+    public Transform joystick;
+    
+	void Start () {
+
+	}
+
+	void Update () {
+        transform.Translate(joystick.localPosition / 200);
+	}
+}
